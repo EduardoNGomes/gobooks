3a573a380e2bcaada7909cb0c10c364e73e23598
Eduardo N Gomes <Eduardo_n_gomes.dev@hotmail.com>
2024-09-01 09:42:36 -0300

    Go routine Exemple

---
+++ cmd/examples/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(workerID int, data chan int) {
+	for x := range data {
+		fmt.Printf("worker %d got %d\n", workerID, x)
+		time.Sleep(time.Second)
+	}
+}
+
+// GO Routines
+func main() {
+	ch := make(chan int)
+	qtdWorkers := 3
+
+	for i := range qtdWorkers {
+		go worker(i, ch)
+
+	}
+
+	for i := range 10 {
+		ch <- i
+	}
+}
